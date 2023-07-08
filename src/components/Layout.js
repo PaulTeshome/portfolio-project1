452cc80f0c8482e3c9907bb7292b3648a4dfc3c8
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-07-08 10:30:31 +0300

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import './Layout.scss'
+import React from 'react'
+import SideBar from './SideBar'
+
+function Layout() {
+  return (
+    <div className='layout'>
+        <SideBar/>
+    </div>
+  )
+}
+
+export default Layout
