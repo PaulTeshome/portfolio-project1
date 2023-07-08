4dd1ef44e7579102e1bd2d17fedb92def8645aff
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-07-08 10:29:57 +0300

    app update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import React from 'react';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <Routes>
+         <Route path="/" element={<Layout/>}/>
+      </Routes>
+    </>
   );
 }
 
