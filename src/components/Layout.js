1b4d34656e09762ac5c09e90cb2bc84e401748c1
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-07-08 11:59:51 +0300

    add Home route

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,14 @@
 import './Layout.scss'
 import React from 'react'
 import SideBar from './SideBar'
+import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
     <div className='App'>
         <SideBar/>
         <div className='page'>
-
+            <Outlet/>
         </div>
     </div>
   )
