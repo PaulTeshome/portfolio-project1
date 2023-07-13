577fbbab1204ccef2be9ed706b1a5939c7756a81
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-07-13 15:54:30 +0300

    Update About.js
    cube animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './About.scss'
 import AnimatedLetters from './AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
 
 function About() {
     const [letterClass,setLetterClass] = useState('text-animate')
@@ -29,7 +31,22 @@ function About() {
         <div className='stage-cube-cont'>
             <div className='cubespinner'>
                 <div className='face1'>
-
+                    <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
+                </div>
+                <div className='face2'>
+                    <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
+                </div>
+                <div className='face3'>
+                    <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
+                </div>
+                <div className='face4'>
+                    <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
+                </div>
+                <div className='face5'>
+                    <FontAwesomeIcon icon={faJsSquare} color="#efd81d"/>
+                </div>
+                <div className='face6'>
+                    <FontAwesomeIcon icon={faGitAlt} color="#ec4d28"/>
                 </div>
             </div>
         </div>
